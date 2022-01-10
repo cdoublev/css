7def48683a5d0911298243c6d79f26b1cd400b3b
Guillaume <gui.lebas@gmail.com>
2022-01-10 14:45:06 +0100

    Create centralized export of CSS interfaces

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 
-// Inheritance order must be taken into account
-const interfaces = [
-    require('./lib/cssom/CSS.js'),
-    require('./lib/cssom/CSSRule.js'),
-    require('./lib/cssom/CSSGroupingRule.js'),
-    require('./lib/cssom/CSSConditionRule.js'),
-    require('./lib/cssom/CSSImportRule.js'),
-    require('./lib/cssom/CSSMarginRule.js'),
-    require('./lib/cssom/CSSMediaRule.js'),
-    require('./lib/cssom/CSSNamespaceRule.js'),
-    require('./lib/cssom/CSSPageRule.js'),
-    require('./lib/cssom/CSSRuleList.js'),
-    require('./lib/cssom/CSSStyleDeclaration.js'),
-    require('./lib/cssom/CSSStyleRule.js'),
-    require('./lib/cssom/CSSSupportsRule.js'),
-    require('./lib/cssom/MediaList.js'),
-    require('./lib/cssom/StyleSheet.js'),
-    require('./lib/cssom/StyleSheetList.js'),
-    require('./lib/cssom/CSSStyleSheet.js'),
-]
+const interfaces = Object.values(require('./lib/cssom/index.js'))
 
-function install(globalObject) {
+/**
+ * @param {DocumentOrShadowRoot} globalObject
+ */
+function install(globalObject = globalThis) {
     interfaces.forEach(wrapper => wrapper.install(globalObject, ['Window']))
 }
 
