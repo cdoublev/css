cdoublev/css

commit 4fd29489adfd0ae8b16f491041859d78e50a6615

Fix parsing <progress-source> and <input-position>
The were 3 problems.

(1) A calculation function replacing the progress source or an input position of
calc-interpolate() was identified as a top-level calculation function whereas
calc-interpolate() is also a calculation function. This was affecting the count
of calculation terms.

(2) Parsing *-interpolate() failed when the progress source or an input position
was replaced with a calculation function whose value was a numeric value. Their
type resolved to null (failure) when trying to validate *-interpolate() because
of an implementation mistake to determine this type.

(3) A calculation function replacing the progress source or an input position was
not simplified, which was presumably an oversight, noting that a resolved value
is now unwrapped in calc-interpolate() but not in other interpolation functions.

Finally, they are now defined with <calc-sum> to simplify their processing, as
suggested in w3c/csswg-drafts#10797, which reverts 89a7f6a.

test/value.js
@@ -2246,6 +2246,9 @@ describe('<calc-mix()>', () => {
 })
 describe('<calc-interpolate()>', () => {
     test('invalid', () => {
+        // Invalid <progress-source> value
+        assert.invalid('<number> | <length>', 'calc-interpolate(auto, 0: 1)')
+        assert.invalid('<number> | <length>', 'calc-interpolate(none, 0: 1)')
         // Invalid <progress-source> type
         assert.invalid('<number> | <length>', 'calc-interpolate(1px, 0: 1)')
         assert.invalid('<length-percentage>', 'calc-interpolate(calc(1% / 1px), 0: 1px)')
@@ -2275,11 +2278,11 @@ describe('<calc-interpolate()>', () => {
             ['<number>', 'calc-interpolate(0%, 0px: 1, 1: 1)'],
             ['<number>', 'calc-interpolate(0px, 0px: 1, 1: 1)'],
             // Type checking <percentage> in nested <progress-source> or <input-position> contexts
-            ['<number>', 'calc-interpolate(progress(0%, 0%, 1%), progress(0%, 0%, 1%): 1)'],
+            ['<number>', 'calc-interpolate(progress(0%, 0%, 1%), progress(0%, 0%, 1%): 1)', 'calc-interpolate(0, 0: 1)'],
             ['<length-percentage>', 'calc-interpolate(progress(0%, 0%, 1%), progress(0%, 0%, 1%): 1px)'],
             ['<length-percentage>', 'calc-interpolate(0%, 0%: 1px)'],
             // Type checking and simplification of <calc-sum> and <calc-interpolate()>
-            ['<length-percentage>', 'calc-interpolate(0%, 0: 1px * 1, 1: 1% + 1px)', 'calc-interpolate(0%, 0: 1px, 1: 1% + 1px)'],
+            ['<length-percentage>', 'calc-interpolate(calc(0%), calc(0): calc(1px * 1), 1: 1% + 1px)', 'calc-interpolate(0%, 0: 1px, 1: 1% + 1px)'],
             ['<length-percentage>', 'calc(1px * calc-interpolate(0%, 0: 1% / 1px, 1: (1% + 1px) / 1px))'],
             // Omitted component values
             ['<number>', 'calc-interpolate(0 by linear linear, 0: 1, linear, 1: 1)', 'calc-interpolate(0, 0: 1, 1: 1)'],
@@ -3357,19 +3360,6 @@ describe('<image-set()>', () => {
         assert.valid('<image-set()>', '-webkit-image-set("image.jpg")', 'image-set("image.jpg")')
     })
 })
-describe('<input-position>', () => {
-    test('invalid', () => {
-        assert.invalid('<input-position>', 'calc((1% + 1px) / 1px)')
-        assert.invalid('<input-position>', 'progress(1%, 1px, 1px)')
-    })
-    test('representation', () => {
-        assert.representation('<input-position>', '50%', percentage(50, ['<input-position>']))
-    })
-    test('valid', () => {
-        assert.valid('<input-position>', 'calc(100% / 2)', 'calc(50%)')
-        assert.valid('<input-position>', 'progress(0%, 0%, 1%)')
-    })
-})
 describe('<keyframe-selector>', () => {
     test('representation', () => {
         assert.representation('<keyframe-selector>', '0%', percentage(0, ['<keyframe-selector>']), keyframeRule)
@@ -3637,22 +3627,6 @@ describe('<position-area-query>', () => {
         assert.valid('<position-area-query>', 'inline-start', 'any start')
         assert.valid('<position-area-query>', 'span-self-block-start', 'span-self-start any')
         assert.valid('<position-area-query>', 'span-self-inline-end', 'any span-self-end')
-    })
-})
-describe('<progress-source>', () => {
-    test('invalid', () => {
-        // Invalid <calc-sum>
-        assert.invalid('<progress-source>', 'calc((1% + 1px) / 1px)')
-        assert.invalid('<progress-source>', 'progress(1%, 1px, 1px)')
-        assert.invalid('<progress-source>', 'auto')
-        assert.invalid('<progress-source>', 'none')
-    })
-    test('representation', () => {
-        assert.representation('<progress-source>', '50%', percentage(50, ['<progress-source>']))
-    })
-    test('valid', () => {
-        assert.valid('<progress-source>', 'calc(100% / 2)', 'calc(50%)')
-        assert.valid('<progress-source>', 'progress(0%, 0%, 1%)')
     })
 })
 describe('<pt-name-and-class-selector>', () => {

lib/parse/simplify.js
@@ -41,9 +41,6 @@ export function simplifyCalculation(node, resolutionType) {
     }
     // Operator: calculation operator or numeric substitution function
     node = map(node, function simplify(value) {
-        if (value.types.includes('<progress-source>') || value.types.includes('<input-position>')) {
-            return value
-        }
         if (isCalculation(value)) {
             return simplifyCalculation(value, resolutionType)
         }

lib/parse/types.js
@@ -336,8 +336,6 @@ function getCalculationType(calculation, resolutionType) {
     const { types, unit, value } = calculation
     for (let index = types.length - 1; 0 <= index; --index) {
         switch (types[index]) {
-            case '<calc-function>':
-                return getCalculationFunctionType(value, resolutionType)
             case '<calc-invert>': {
                 const type = getCalculationType(value, resolutionType)
                 return type ? invert(type) : null

lib/values/definitions.js
@@ -260,7 +260,7 @@ export default {
     '<inflexible-breadth>': '<length-percentage [0,∞]> | min-content | max-content | auto',
     '<inherit()>': 'inherit(<custom-property-name> , <declaration-value>?)',
     '<inherit-args>': 'inherit(<declaration-value> , <declaration-value>?)',
-    '<input-position>': '<percentage> | <number> | <dimension>',
+    '<input-position>': '<calc-sum>',
     '<inset()>': "inset(<length-percentage>{1,4} [round <'border-radius'>]?)",
     '<integer>': '<number-token>',
     '<integrity-modifier>': 'integrity(<string>)',
@@ -392,7 +392,7 @@ export default {
     '<predefined-rgb-params>': '<predefined-rgb> [<number> | <percentage> | none]{3}',
     '<predefined-rgb>': 'srgb | srgb-linear | display-p3 | display-p3-linear | a98-rgb | prophoto-rgb | rec2020 | rec2100-pq | rec2100-hlg | rec2100-linear',
     '<progress()>': 'progress(<calc-sum> , <calc-sum> , <calc-sum>)',
-    '<progress-source>': "<percentage> | <number> | <dimension> | <'animation-timeline'>",
+    '<progress-source>': "<calc-sum> | <'animation-timeline'>",
     '<pseudo-class-selector>': ': <ident-token> | : <function-token> <any-value> )',
     '<pseudo-compound-selector>': '<pseudo-element-selector> <pseudo-class-selector>*',
     '<pseudo-element-selector>': ': <pseudo-class-selector> | <legacy-pseudo-element-selector>',

scripts/extract.js
@@ -201,6 +201,9 @@ const replaced = {
         '<feature-index>': '<integer>',
         // https://github.com/w3c/csswg-drafts/issues/13010
         '<event-trigger-event>': 'activate | click | touch | dblclick | keypress(<string>)',
+        // https://github.com/w3c/csswg-drafts/issues/10797
+        '<input-source>': "<calc-sum>",
+        '<progress-source>': "<calc-sum> | <'animation-timeline'>",
         // https://github.com/w3c/fxtf-drafts/issues/532
         '<mask-reference>': 'none | <image>',
         // TODO: fix `value` of `<pseudo-page>`

commit 4415c43d8e40981eefa0c68d6e5b3667a7d2b760

Oversights in 7b0acf5 and 4fd2948
After the refactoring to replace Jest with the NodeJS test framework in 7b0acf5,
some tests no longer used the context to parse their value.

Reusing this context revealed a regression in 4fd2948. A calculation function
replacing <progress-source> and <input-position> was no longer type checked
because it was no longer identified as top-level (which is legitimate).

lib/parse/replace.js
@@ -125,6 +125,16 @@ function replaceNumeric(node) {
             }
             return null
         }
+        if (findParent(node, ({ definition: { name } }) => name === '<input-position>' || name === '<progress-source>')) {
+
+            const replacedType = getReplacedNumericType(node)
+            const resolutionType = getPercentageResolutionType(node)
+            const type = getCalculationType(match, resolutionType)
+
+            if (!matchNumericType(type, replacedType, resolutionType)) {
+                return null
+            }
+        }
         return match
     }
     return null

test/value.js
@@ -888,23 +888,23 @@ describe('<declaration-value>', () => {
 describe('<declaration>', () => {
     test('invalid', () => {
         // Top-level or nested <bad-*-token>, ), ], }
-        assert.invalid('<declaration>', 'color: var(--custom) fn("\n)')
-        assert.invalid('<declaration>', 'color: var(--custom) (url(bad .url))')
-        assert.invalid('<declaration>', 'color: var(--custom) [)]')
-        assert.invalid('<declaration>', 'color: var(--custom) (])')
-        assert.invalid('<declaration>', 'color: var(--custom) (})')
-        assert.invalid('<declaration>', '--custom: fn("\n)')
-        assert.invalid('<declaration>', '--custom: (url(bad .url))')
-        assert.invalid('<declaration>', '--custom: [)]')
-        assert.invalid('<declaration>', '--custom: (])')
-        assert.invalid('<declaration>', '--custom: (})')
+        assert.invalid('<declaration>', 'color: var(--custom) fn("\n)', styleRule)
+        assert.invalid('<declaration>', 'color: var(--custom) (url(bad .url))', styleRule)
+        assert.invalid('<declaration>', 'color: var(--custom) [)]', styleRule)
+        assert.invalid('<declaration>', 'color: var(--custom) (])', styleRule)
+        assert.invalid('<declaration>', 'color: var(--custom) (})', styleRule)
+        assert.invalid('<declaration>', '--custom: fn("\n)', styleRule)
+        assert.invalid('<declaration>', '--custom: (url(bad .url))', styleRule)
+        assert.invalid('<declaration>', '--custom: [)]', styleRule)
+        assert.invalid('<declaration>', '--custom: (])', styleRule)
+        assert.invalid('<declaration>', '--custom: (})', styleRule)
         // Top-level ; and !
-        assert.invalid('<declaration>', 'color: var(--custom) ;')
-        assert.invalid('<declaration>', 'color: var(--custom) !')
-        assert.invalid('<declaration>', '--custom: ;')
+        assert.invalid('<declaration>', 'color: var(--custom) ;', styleRule)
+        assert.invalid('<declaration>', 'color: var(--custom) !', styleRule)
+        assert.invalid('<declaration>', '--custom: ;', styleRule)
         // Positioned {} block
-        assert.invalid('<declaration>', 'color: var(--custom) {}')
-        assert.invalid('<declaration>', 'color: {} var(--custom)')
+        assert.invalid('<declaration>', 'color: var(--custom) {}', styleRule)
+        assert.invalid('<declaration>', 'color: {} var(--custom)', styleRule)
     })
     test('representation', () => {
         const declaration = {
@@ -2246,29 +2246,32 @@ describe('<calc-mix()>', () => {
 })
 describe('<calc-interpolate()>', () => {
     test('invalid', () => {
-        // Invalid <progress-source> value
-        assert.invalid('<number> | <length>', 'calc-interpolate(auto, 0: 1)')
-        assert.invalid('<number> | <length>', 'calc-interpolate(none, 0: 1)')
-        // Invalid <progress-source> type
-        assert.invalid('<number> | <length>', 'calc-interpolate(1px, 0: 1)')
-        assert.invalid('<length-percentage>', 'calc-interpolate(calc(1% / 1px), 0: 1px)')
-        assert.invalid('<length-percentage>', 'calc-interpolate(calc((1% + 1px) / 1px), 0: 1px)')
-        assert.invalid('<length-percentage>', 'calc-interpolate(progress(1%, 1px, 1px), 0: 1px)')
-        // Invalid <input-position> type
-        assert.invalid('<length-percentage>', 'calc-interpolate(0, calc(1% / 1px): 0px)')
-        assert.invalid('<length-percentage>', 'calc-interpolate(0, calc((1% + 1px) / 1px): 0px)')
-        assert.invalid('<length-percentage>', 'calc-interpolate(0, progress(1%, 1px, 1px): 0px)')
-        // Missing absolute <input-position> type
-        assert.invalid('<length>', 'calc-interpolate(1px, 0: 1px)')
-        // Inconsistent absolute <progress-source> and <input-position> types
-        assert.invalid('<length>', 'calc-interpolate(0deg, 0px: 1px, 1px: 1px)')
-        assert.invalid('<length>', 'calc-interpolate(0px, 0deg: 1px, 1px: 1px)')
-        // Inconsistent calculation types
-        assert.invalid('<number> | <length>', 'calc-interpolate(0, 0: 1, 1: 1px)')
-        assert.invalid('<number> | <percentage>', 'calc-interpolate(0, 0: 1, 1: 1%)')
-        // Result type mismatch
-        assert.invalid('<number> | <percentage>', 'calc-interpolate(0, 0: (1% + 1px) / 1px)')
-        assert.invalid('<length>', 'calc-interpolate(0, 0: 1% + 1px)')
+        const invalid = [
+            // Invalid <progress-source> value
+            ['<number> | <length>', 'calc-interpolate(auto, 0: 1)'],
+            ['<number> | <length>', 'calc-interpolate(none, 0: 1)'],
+            // Invalid <progress-source> type
+            ['<number> | <length>', 'calc-interpolate(1px, 0: 1)'],
+            ['<length-percentage>', 'calc-interpolate(calc(1% / 1px), 0: 1px)'],
+            ['<length-percentage>', 'calc-interpolate(calc((1% + 1px) / 1px), 0: 1px)'],
+            ['<length-percentage>', 'calc-interpolate(progress(1%, 1px, 1px), 0: 1px)'],
+            // Invalid <input-position> type
+            ['<length-percentage>', 'calc-interpolate(0, calc(1% / 1px): 0px)'],
+            ['<length-percentage>', 'calc-interpolate(0, calc((1% + 1px) / 1px): 0px)'],
+            ['<length-percentage>', 'calc-interpolate(0, progress(1%, 1px, 1px): 0px)'],
+            // Missing absolute <input-position> type
+            ['<length>', 'calc-interpolate(1px, 0: 1px)'],
+            // Inconsistent absolute <progress-source> and <input-position> types
+            ['<length>', 'calc-interpolate(0deg, 0px: 1px, 1px: 1px)'],
+            ['<length>', 'calc-interpolate(0px, 0deg: 1px, 1px: 1px)'],
+            // Inconsistent calculation types
+            ['<number> | <length>', 'calc-interpolate(0, 0: 1, 1: 1px)'],
+            ['<number> | <percentage>', 'calc-interpolate(0, 0: 1, 1: 1%)'],
+            // Result type mismatch
+            ['<number> | <percentage>', 'calc-interpolate(0, 0: (1% + 1px) / 1px)'],
+            ['<length>', 'calc-interpolate(0, 0: 1% + 1px)'],
+        ]
+        invalid.forEach(([definition, input]) => assert.invalid(definition, input, styleRule))
     })
     test('valid', () => {
         const valid = [
@@ -2295,12 +2298,15 @@ describe('<calc-interpolate()>', () => {
 })
 describe('<random()>', () => {
     test('invalid', () => {
-        // Inconsistent calculation types
-        assert.invalid('<number> | <length>', 'random(1, 1px, 1)')
-        assert.invalid('<number> | <percentage>', 'random(1, 1%, 1)')
-        // Result type mismatch
-        assert.invalid('<number> | <percentage>', 'random(1, (1% + 1px) / 1px, 1)')
-        assert.invalid('<length>', 'random(1px, 1%, 1px)')
+        const invalid = [
+            // Inconsistent calculation types
+            ['<number> | <length>', 'random(1, 1px, 1)'],
+            ['<number> | <percentage>', 'random(1, 1%, 1)'],
+            // Result type mismatch
+            ['<number> | <percentage>', 'random(1, (1% + 1px) / 1px, 1)'],
+            ['<length>', 'random(1px, 1%, 1px)'],
+        ]
+        invalid.forEach(([definition, input]) => assert.invalid(definition, input, styleRule))
     })
     test('valid', () => {
         assert.valid('<number>', 'RANDOM(auto, 1 / 1, 1em / 1px)', 'random(1, 1em / 1px)', styleRule)